32ead7d5f41287b603b30f247631bb8b96018500
Никита <nikitarodionovasfrid@yandex.ru>
2013-07-10 10:07:47 +0600

    as

--- Songs/song/all_chords.py
+++ Songs/song/all_chords.py
@@ -2,10 +2,33 @@
 
 from itertools import product
 
-all_chord_types = ["", "m", "7", "m7", "m+7", "maj7", "+7", "m7b5",
-                   "dim7", "sus2", "7sus2", "sus4", "7sus4", "sus",
-                   "5", "6", "m6", "6/9", "m6/9", "9", "11", "13",
-                   "add9", "madd9"]
+
+all_chord_types = {
+    "": (0, 4, 7),
+    "7sus2": (0, 2, 7),
+    "m": (0, 3, 7),
+    "7": (0, 4, 8, 11),
+    "m7": (0, 3, 7, 10),
+    "maj7": (0, 4, 7, 11),
+    "m+7": (0, 3, 7, 11),
+    "m7b5": (0, 3, 6, 10),
+    "dim7": (0, 3, 6, 9),
+    "sus2": (0, 2, 7),
+    "sus4": (0, 5, 7),
+    "7sus4": (0, 5, 7, 10),
+    "sus": (0, 5, 7),
+    "5": (0, 5),
+    "dim": (0, 3, 6),
+    "6": (0, 4, 7, 16),
+    "m6": (0, 3, 7, 16),
+    "6/9": (0, 4, 7, 11, 16),
+    "m6/9": (0, 3, 7, 16, 30),
+    "9": (0, 4, 7, 10),
+    "11": (0, 4, 7, 10, 14, 17),
+    "13": (0, 4, 7, 10, 14, 19),
+    "add9": (0, 4, 7, 21),
+    "madd9": (0, 4, 7, 21)
+}
 
 
 
@@ -78,3 +101,30 @@ def parse_chord(chord):
         mod = chord[1:]
 
     return tone, mod, add
+
+
+def semitone_distance(first_chord, second_chord):
+    "Возвращаем число полутонов от первого до 2-го аккорда"
+    dist = (tones_indexed[get_tone(normal_view(second_chord))] - tones_indexed[get_tone(normal_view(first_chord))]) % 12
+    if dist < 0:
+        dist += 12
+    return dist
+
+
+def shift_tone(base_tone, distance):
+    t = (tones_indexed[base_tone] + distance) % 12
+    return all_chord_tones[t]
+
+
+def musicals(chord):
+    """Принимаем аккорд с модификацией, тоном и дополнительной нотой,
+    и возвращаем ноты аккорда.
+    :param chord:
+    """
+    tone, mod, note = parse_chord(chord)
+    if note:
+        dist = semitone_distance(tone, note)
+        all_chord_types[mod] = list(all_chord_types[mod])
+        all_chord_types[mod].append(dist)                         # добавляем ноту к модификации
+        tuple(all_chord_types[mod])
+    return [shift_tone(tone, x) for x in all_chord_types[mod]]
---
+++ Songs/song/drawer.py
@@ -0,0 +1,51 @@
+#-*- coding: utf-8 -*-
+from PIL import Image, ImageDraw
+
+coordinate = {
+    "x": [0, 0],
+    0: [-100, 0],
+    1: [70, 4],
+    2: [210, 4],
+    3: [350, 4],
+    4: [490, 4]
+}
+ladi = {
+
+}
+
+
+# создаем апликатуру
+def aplicatura(spisok):
+    # Рисуем палец
+    finger = Image.new("RGBA", (35, 35), (0, 0, 0, 0))
+    draw = ImageDraw.Draw(finger)
+    draw.ellipse((0, 0, 35, 35), fill="red", outline="red")
+
+    # рисуем бэре
+    bere = Image.new("RGBA", (75, 245), (0, 0, 0, 0))
+    draw = ImageDraw.Draw(bere)
+    draw.ellipse((25, 0, 50, 245), fill="red", outline="red")
+
+    # рисуем cross
+    cross = Image.new("RGBA", (35, 35), (0, 0, 0, 0))
+    draw = ImageDraw.Draw(cross)
+    draw.line((0, 0, 35, 35), fill="red", width=7)
+    draw.line((35, 0, 0, 35), fill="red", width=7)
+
+    aplic = Image.open("Grif.png")
+    draw = ImageDraw.Draw(aplic)
+    spisok = list(spisok)
+    min_lad = min(spisok)
+    for i, lad in enumerate(spisok):
+        lad -= (min_lad - 1)
+        x = coordinate[lad][0]
+        y = coordinate[lad][1] + 203.5 - i * 41.5
+        if lad == 'x':
+            draw.bitmap((x, y), cross, fill="red")
+        else:
+            draw.bitmap((x, y), finger, fill="red")
+        aplic.save("aplicatura.png", "PNG")
+    del draw
+        # aplic.save(sys.stdout, "JPEG")
+
+aplicatura(spisok=(2, 3, 2, 4, 5, 3))
